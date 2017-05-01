6c49c466efa39848480c0e7b23bcb27eef1b5001
Avi Avni <avi.avni@gmail.com>
2017-05-01 03:05:38 +0300

    module 1 demo code

--- FSharpCourse/FSharpCourse.Module01/Program.fs
+++ FSharpCourse/FSharpCourse.Module01/Program.fs
@@ -1,7 +1,34 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿let add a b = a + b
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let first (x, y) = x
+
+let map (f : 'a -> 'b) (xs : 'a seq) =
+    seq { for x in xs do
+              yield f x }
+
+let newList = [1..10] 
+              |> map (fun x -> x + 1)
+
+let add1 = add 1
+let value = add1 2
+
+type Person() =
+    do 
+        printfn "Person created"
+
+let num = 1
+let str = "Hii"
+let person = Person()
+
+let rec factorial n =
+    if n = 0 then
+        1
+    else n * factorial (n - 1)
+
+let tailRecFactorial n =
+    let rec fact n acc =
+        if n = 0 then
+            acc
+        else
+            fact (n - 1) acc * n
+    fact n 1
