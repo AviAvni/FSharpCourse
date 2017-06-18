dc59f405419fe2ff8445eedeb5a435fdf8620012
Avi Avni <avi.avni@gmail.com>
2017-06-18 15:45:33 +0300

    add demos

--- FSharpCourse/FSharpCourse.Module05/Program.fs
+++ FSharpCourse/FSharpCourse.Module05/Program.fs
@@ -2,6 +2,9 @@
 open FsXaml
 open System.Windows
 open System.Windows.Controls
+open System.Reactive.Linq
+open System.Reactive.Concurrency
+open System.Threading
 
 type MainWindow = XAML<"MainWindow.xaml">
 
@@ -26,5 +29,51 @@ let main _ =
     //|> Observable.add (fun a -> MessageBox.Show "Hii"
     //                            |> ignore)
 
+    printfn "Interval"
+    let observable = Observable.Interval(TimeSpan.FromSeconds(1.0))
+
+    let subscription = observable.Subscribe (printfn "%d")
+
+    Console.ReadLine()
+
+    subscription.Dispose()
+
+    printfn "Buffer" 
+    let observable = Observable.Interval(TimeSpan.FromSeconds(1.0)).Buffer(3)
+
+    let subscription = observable.Subscribe (printfn "%A")
+
+    Console.ReadLine()
+
+    subscription.Dispose()
+
+    printfn "Window"
+    let observable = Observable.Interval(TimeSpan.FromSeconds(1.0)).Window(3)
+
+    let subscription = observable.Subscribe (fun obs -> obs.Sum().Subscribe (printfn "%d") |> ignore)
+
+    Console.ReadLine()
+
+    subscription.Dispose()
+
+    printfn "GroupBy"
+    let observable = Observable.Interval(TimeSpan.FromSeconds(1.0)).GroupBy(fun x -> x % 3L)
+
+    let printKeyValue key value =
+        printfn "%s %d" (String('*', key)) value
+        ()
+        
+    let subscription = observable.Subscribe (fun obs -> obs.Subscribe (printKeyValue (int obs.Key)) |> ignore)
+
+    Console.ReadLine()
+
+    subscription.Dispose()
+
+    let txt : TextBox = win.txt
+    let console : TextBlock = win.console
+
+    let observable = Observable.FromEventPattern(txt, "TextChanged").Throttle(TimeSpan.FromSeconds(1.0))
+
+    let subscription = observable.ObserveOn(SynchronizationContext.Current).Subscribe (fun text -> console.Text <-console.Text + Environment.NewLine + txt.Text )
 
     app.Run win
