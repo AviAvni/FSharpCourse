bfefde4dd373888e78b1614cf801e444ebb017c4
Avi Avni <avi.avni@gmail.com>
2017-11-30 14:07:00 +0200

    add chart

--- FSharpCourse/FSharpCourse.Module03/SimpleChart.fs
+++ FSharpCourse/FSharpCourse.Module03/SimpleChart.fs
@@ -3,6 +3,7 @@
 open System
 open FSharp.Charting
 open System.Reactive.Linq
+open FSharp.Charting._ChartStyleExtensions
 
 
 let list = [1..10]
@@ -31,4 +32,21 @@ Chart.Combine(
         LiveChart.LineIncremental obs2
     ])
 |> Chart.WithLegend true
+|> Chart.WithXAxis(Min = -2.0, Max = 2.0)
+|> Chart.WithYAxis(Min = -2.0, Max = 2.0)
+|> Chart.Show
+
+let obs3 = Observable
+            .Interval(TimeSpan.FromMilliseconds(10.0))
+            .Select(fun x -> let rad = (float x)*Math.PI/180.0
+                             16.0 * (pown (sin rad) 3), 13.0 * (cos rad) -  5.0 * (cos (2.0 * rad)) -  2.0 * (cos (3.0 * rad)) - cos (4.0 * rad))
+            .Take(360)
+
+Chart.Combine(
+    [
+        LiveChart.LineIncremental obs3
+    ])
+|> Chart.WithLegend true
+|> Chart.WithXAxis(Min = -20.0, Max = 20.0)
+|> Chart.WithYAxis(Min = -20.0, Max = 20.0)
 |> Chart.Show
